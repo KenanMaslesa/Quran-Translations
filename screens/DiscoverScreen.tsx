475e0f7d6cb33035ce29e3a81d30ad1c44302692
Kenan Maslesa <kenoo.maslesa@gmail.com>
2022-06-26 16:17:10 +0200

    .

--- screens/DiscoverScreen.tsx
+++ screens/DiscoverScreen.tsx
@@ -1,11 +1,62 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  FlatList,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import AyahCard from "../components/AyahCard";
+import { Ayah } from "../models";
+
+const quranTranslation = require("@kmaslesa/quran-translations");
 
 const DiscoverScreen = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [ayahs, setAyahs] = useState<Ayah[]>([]);
+
+  useEffect(() => {
+    const response = quranTranslation.searchAyahs(searchTerm);
+    setAyahs(response);
+  }, [searchTerm]);
+
   return (
-    <View>
-      <Text>DiscoverScreen</Text>
-    </View>
+    <ImageBackground source={require("../assets/background.jpg")}>
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder="Search Qur'an"
+          placeholderTextColor={"white"}
+          value={searchTerm}
+          onChangeText={setSearchTerm}
+        />
+        <Text style={styles.text}>Found: {ayahs.length}</Text>
+        <FlatList
+          data={ayahs}
+          keyExtractor={(item) => `${item.index}`}
+          initialNumToRender={7}
+          renderItem={({ item }) => <AyahCard ayah={item} />}
+        />
+      </View>
+    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "gray",
+    borderRadius: 5,
+    color: "white",
+  },
+  text: {
+    color: "white",
+    textAlign: "center",
+  },
+});
 
 export default DiscoverScreen;
